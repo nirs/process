83750f965911336b3807d49fbb20dacb90c785d7
Nir Soffer <nirsof@gmail.com>
2025-03-15 18:57:46 +0200

    Import files

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nirs/process
+
+go 1.23.0
+
+require github.com/mitchellh/go-ps v1.0.0
---
+++ process.go
@@ -0,0 +1,111 @@
+/*
+Copyright 2025 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package process
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"github.com/mitchellh/go-ps"
+)
+
+const pidfileMode = 0o600
+
+// WritePidfile writes pid to path.
+func WritePidfile(path string, pid int) error {
+	data := fmt.Sprintf("%d", pid)
+	return os.WriteFile(path, []byte(data), pidfileMode)
+}
+
+// ReadPid reads a pid from path.
+func ReadPidfile(path string) (int, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return -1, err
+	}
+	s := strings.TrimSpace(string(data))
+	pid, err := strconv.Atoi(s)
+	if err != nil {
+		return -1, err
+	}
+	return pid, nil
+}
+
+// Exists tells if a process with pid and executable name exist. Executable is
+// not the path to the executable.
+func Exists(pid int, executable string) (bool, error) {
+	// Fast path if pid does not exist.
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return true, err
+	}
+	if err := process.Signal(syscall.Signal(0)); err != nil {
+		if err == os.ErrProcessDone {
+			return false, nil
+		}
+	}
+	// Slow path, if pid exist.
+	entry, err := ps.FindProcess(pid)
+	if err != nil {
+		return true, err
+	}
+	if entry == nil {
+		return false, nil
+	}
+	return entry.Executable() == executable, nil
+}
+
+// Signal sends signal to the process with pid matching name. Returns
+// os.ErrProcessDone if the process does not exist, or nil if the signal was
+// sent.
+func Signal(pid int, executable string, sig syscall.Signal) error {
+	exists, err := Exists(pid, executable)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return os.ErrProcessDone
+	}
+	p, err := os.FindProcess(pid)
+	if err != nil {
+		return err
+	}
+	// Returns os.ErrProcessDone if process does not exist (ESRCH).
+	return p.Signal(sig)
+}
+
+// Terminate kills a process with pid matching executable name. Returns
+// os.ErrProcessDone if the process does not exist or nil the kill was
+// requested.
+func Kill(pid int, executable string) error {
+	exists, err := Exists(pid, executable)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return os.ErrProcessDone
+	}
+	p, err := os.FindProcess(pid)
+	if err != nil {
+		return err
+	}
+	// Returns os.ErrProcessDone if process does not exist (ESRCH).
+	return p.Kill()
+}
